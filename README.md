78b3081329cee167b49c9f58695cba188ef421e3
Endri Gjiri <egjiri@gmail.com>
2014-02-09 17:46:24 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+## JRuby & Rails Test App
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = '6579d9d6ff92fecbdc0f6eaf467422d3d0440d373133d7f0da9c95727aabe56c541ded02ba3da4141e724bab396d75a9543b4125ccb2d00e04d6779b9c1fcf5a'
